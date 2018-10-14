a5220b4de6dca6a8dc9d193ce78b399520a857b8
xpollen8 <github@xpollen8.com>
2018-10-14 09:47:33 -0700

    Update process.sh
    Fixed a tpyo

--- src/process.sh
+++ src/process.sh
@@ -5,7 +5,7 @@ file=$1
 
 if [ -f $file ]; then
 	dir=$( echo $file | sed -e 's/\.wav$//')
-	echo "Processing: $$file.."
+	echo "Processing: $file.."
 	java -jar PxlMagic.jar $file
 	echo "Extracting $dir/$outVid.."
 	(cd $dir && ffmpeg -r 15 -f image2 -i image%05d.png -i final_audio.wav $outVid)
